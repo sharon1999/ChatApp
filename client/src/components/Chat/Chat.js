488810f424529f533c3b878d4cbc0c4c9b5ae254
sharon1999 <sharonantony51@gmail.com>
2023-04-17 22:25:18 +0530

    UI Changes

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -5,16 +5,17 @@ import "./Chat.css";
 import InfoBar from "../InfoBar/Infobar";
 import Input from "../Input/Input";
 import Messages from "../Messages/Messages";
+import onlineIcon from '../../icons/onlineIcon.png';
 import ChatList from "../ChatList/ChatList";
 
 let socket;
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
-  const [users, setUsers] = useState('');
+  const [users, setUsers] = useState("");
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = "http://localhost:5000";
+  const ENDPOINT = "https://chat-app-reactjs.onrender.com/";
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
@@ -49,7 +50,7 @@ const Chat = ({ location }) => {
   console.log("Msgs", messages);
   return (
     <div className="outerContainer">
-      <div className="container">
+      <div className="left-container">
         <InfoBar room={room} />
         <Messages messages={messages} name={name} />
         <Input
@@ -58,7 +59,26 @@ const Chat = ({ location }) => {
           sendMessage={sendMessage}
         />
       </div>
-      <div><ChatList users={users} /></div>
+        {/* <ChatList users={users} />*/}
+        <div className="right-container">
+    {
+      users
+        ? (
+          <div>
+            <h1 className='text-lg p-5'>People currently chatting:</h1>
+              <h2>
+                {users.map(({name}) => (
+                  <div key={name} className="activeItem flex justify-center align-middle">
+                    <img alt="Online Icon" className='px-2' src={onlineIcon}/>
+                    {name}
+                  </div>
+                ))}
+              </h2>
+          </div>
+        )
+        : null
+    }
+  </div>
     </div>
   );
 };
